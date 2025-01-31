20aea8a5527c3d0723077955ae38a3d9546a7b67
Eric <cheng.eric11@gmail.com>
2025-01-31 15:04:05 -0500

    Update df_dataAttribute_analysis.py

--- src/data_analysis/df_dataAttribute_analysis.py
+++ src/data_analysis/df_dataAttribute_analysis.py
@@ -1,47 +1,27 @@
 import pandas as pd
 
-def categorize_columns(df):
+def get_data_attributes(df):
     # Initialize arrays to hold column names
     categories = {
         'categorical': [],
         'numerical': [],
-        'miscellaneous': []
+        'miscellaneous': [],
+        'boolean': []
     }
 
     # Iterate through each column and categorize based on its dtype
     for column in df.columns:
         dtype = df[column].dtype
 
-        if pd.api.types.is_object_dtype(dtype) or pd.api.types.is_bool_dtype(dtype):  # Treat booleans as categorical
+        if pd.api.types.is_bool_dtype(dtype):
+            categories['boolean'].append(column)
+            categories['categorical'].append(column)
+        elif pd.api.types.is_object_dtype(dtype):  # Treat booleans as categorical
             categories['categorical'].append(column)
         elif pd.api.types.is_float_dtype(dtype):
             categories['numerical'].append(column)
         else:
             categories['miscellaneous'].append(column)
 
-    return categories
-
-
-# def categorize_columns(df):
-#     # Initialize arrays to hold column names
-#     categories = {
-#         'categorical': [],
-#         'numerical': [],
-#         'boolean': [],
-#         'miscellaneous': []
-#     }
-
-#     # Iterate through each column and categorize based on its dtype
-#     for column in df.columns:
-#         dtype = df[column].dtype
-
-#         if pd.api.types.is_object_dtype(dtype):
-#             categories['categorical'].append(column)
-#         elif pd.api.types.is_float_dtype(dtype):
-#             categories['numerical'].append(column)
-#         elif pd.api.types.is_bool_dtype(dtype):
-#             categories['boolean'].append(column)
-#         else:
-#             categories['miscellaneous'].append(column)
-
-#     return categories
+    return categories#asdasd
+#test
